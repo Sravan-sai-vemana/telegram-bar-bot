a926bbd17b051b68c4393c222a85e1d6159dc207
Sravan-sai-vemana <136155265+Sravan-sai-vemana@users.noreply.github.com>
2023-06-10 13:16:19 +0530

    Create bardbot.js

---
+++ bardbot.js
@@ -0,0 +1,30 @@
+const hostname = '0.0.0.0'
+const port = 3000
+const TelegramBot = require('node-telegram-bot-api'); 
+const token = '5883500954:AAFao11nsfgYlibL7rTzb1NLXJx8oQpJ3Ew';
+const bot = new TelegramBot(token, {polling: true});
+
+const { BardAPI } = require('bard-api-node');
+
+const assistant = new BardAPI();
+
+async function MakeAssistant() {
+  try {
+    await assistant.setSession('__Secure-1PSID', 'WgjdhzNPOlGr_RuporRQddP4HoYrwYyMAJHU3mnVJWhiVv1rmbEuW2_jiuvT4Xy7y-AVew.');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+MakeAssistant();
+async function Response(text,msg) {
+    try {
+      const response = await assistant.getBardResponse(text);
+      bot.sendMessage(msg.chat.id, response.content);
+    } catch (error) {
+      bot.sendMessage(msg.chat.id,"I'm unable to help you with that, as I'm only a language model and don't have the necessary information or abilities.");
+    }
+  }
+
+bot.on('message', function(msg){
+    Response(msg.text,msg);
+})
